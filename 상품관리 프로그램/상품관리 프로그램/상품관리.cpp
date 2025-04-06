c2cf326fd0b6eb11fe26f8e2c7e09e805d9e8672
Naiery0 <molly724@naver.com>
2025-04-06 21:50:06 +0900

    Update 상품관리.cpp

--- 상품관리 프로그램/상품관리 프로그램/상품관리.cpp
+++ 상품관리 프로그램/상품관리 프로그램/상품관리.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
-// 기본 상품 클래스
+// 상품
 class Product {
 protected:
     string id;
@@ -13,14 +14,18 @@ public:
     Product(string id, int price, string producer)
         : id(id), price(price), producer(producer) {}
 
-    virtual void showInfo() {
-        cout << "ID: " << id << ", Price: " << price << ", Producer: " << producer;
+    string getId() const { return id; }
+
+    virtual void showInfo() const {
+        cout << "ID: " << id << ", 가격: " << price << ", 제조사: " << producer;
     }
+
+    virtual string getType() const = 0;
 
     virtual ~Product() {}
 };
 
-// 책 클래스
+// 책
 class Book : public Product {
 private:
     string title;
@@ -30,61 +35,182 @@ public:
     Book(string id, int price, string producer, string title, string author)
         : Product(id, price, producer), title(title), author(author) {}
 
-    void showInfo() override {
+    void showInfo() const override {
         Product::showInfo();
-        cout << ", Title: " << title << ", Author: " << author << endl;
+        cout << ", 책 제목: " << title << ", 저자: " << author << endl;
+    }
+
+    string getType() const override {
+        return "Book";
     }
 };
 
-// 핸드폰 클래스
+// 핸드폰
 class HandPhone : public Product {
 private:
     string model;
-    int storage;
 
 public:
-    HandPhone(string id, int price, string producer, string model, int storage)
-        : Product(id, price, producer), model(model), storage(storage) {}
+    HandPhone(string id, int price, string producer, string model)
+        : Product(id, price, producer), model(model) {}
 
-    void showInfo() override {
+    void showInfo() const override {
         Product::showInfo();
-        cout << ", Model: " << model << ", Storage: " << storage << "GB" << endl;
+        cout << ", 모델명: " << model << endl;
+    }
+
+    string getType() const override {
+        return "HandPhone";
     }
 };
 
-// 컴퓨터 클래스
+
+// 컴퓨터
 class Computer : public Product {
 private:
     string cpu;
-    int ram;
 
 public:
-    Computer(string id, int price, string producer, string cpu, int ram)
-        : Product(id, price, producer), cpu(cpu), ram(ram) {}
+    Computer(string id, int price, string producer, string cpu)
+        : Product(id, price, producer), cpu(cpu) {}
 
-    void showInfo() override {
+    void showInfo() const override {
         Product::showInfo();
-        cout << ", CPU: " << cpu << ", RAM: " << ram << "GB" << endl;
+        cout << ", CPU모델명: " << cpu << endl;
+    }
+
+    string getType() const override {
+        return "Computer";
     }
 };
 
-// 메인 함수
-int main() {
-    vector<Product*> products;
 
-    // 상품 등록
-    products.push_back(new Book("B001", 15000, "BookPub", "C++ Programming", "Kim"));
-    products.push_back(new HandPhone("H001", 800000, "Samsung", "Galaxy S21", 128));
-    products.push_back(new Computer("C001", 1200000, "LG", "Intel i7", 16));
+// 상품 목록 저장
+vector<Product*> products;
 
-    // 상품 정보 출력
+// 추가
+void addProduct() {
+    int type;
+    cout << "\n상품 종류 선택 (1: 책, 2: 핸드폰, 3: 컴퓨터): ";
+    cin >> type;
+
+    string id, producer;
+    int price;
+    cout << "ID: ";
+    cin >> id;
+    cout << "가격: ";
+    cin >> price;
+    cout << "제조사: ";
+    cin >> producer;
+
+    if (type == 1) {
+        string title, author;
+        cout << "책 제목: ";
+        cin.ignore(); getline(cin, title);
+        cout << "저자: ";
+        getline(cin, author);
+        products.push_back(new Book(id, price, producer, title, author));
+    }
+    else if (type == 2) {
+        string model;
+        cout << "모델명: ";
+        cin >> model;
+        products.push_back(new HandPhone(id, price, producer, model));
+    }
+    else if (type == 3) {
+        string cpu;
+        cout << "CPU모델명: ";
+        cin >> cpu;
+        products.push_back(new Computer(id, price, producer, cpu));
+    }
+    else {
+        cout << "잘못된 입력입니다." << endl;
+    }
+
+    cout << "상품이 추가되었습니다." << endl;
+}
+
+// 검색
+void searchProduct() {
+    string searchId;
+    cout << "\n검색할 상품 ID 입력: ";
+    cin >> searchId;
+
+    bool found = false;
+    for (Product* p : products) {
+        if (p->getId() == searchId) {
+            cout << "\n상품 정보:" << endl;
+            p->showInfo();
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "해당 ID의 상품이 없습니다." << endl;
+    }
+}
+
+// 삭제
+void deleteProduct() {
+    string deleteId;
+    cout << "\n삭제할 상품 ID 입력: ";
+    cin >> deleteId;
+
+    for (auto it = products.begin(); it != products.end(); ++it) {
+        if ((*it)->getId() == deleteId) {
+            delete* it;
+            products.erase(it);
+            cout << "\n상품이 삭제되었습니다." << endl;
+            return;
+        }
+    }
+
+    cout << "해당 ID의 상품이 없습니다." << endl;
+}
+
+// 전체 상품 출력
+void listProducts() {
+    cout << "\n[전체 상품 목록]" << endl;
     for (Product* p : products) {
         p->showInfo();
     }
+}
 
-    // 메모리 해제
-    for (Product* p : products) {
-        delete p;
+int main() {
+    int choice;
+
+    while (true) {
+        cout << "\n===== 상품 관리 프로그램 =====\n" << endl;
+        cout << "1. 상품 추가" << endl;
+        cout << "2. 상품 검색" << endl;
+        cout << "3. 상품 삭제" << endl;
+        cout << "4. 전체 상품 출력" << endl;
+        cout << "0. 종료" << endl;
+        cout << "선택: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            addProduct();
+        }
+        else if (choice == 2) {
+            searchProduct();
+        }
+        else if (choice == 3) {
+            deleteProduct();
+        }
+        else if (choice == 4) {
+            listProducts();
+        }
+        else if (choice == 0) {
+            for (Product* p : products)
+                delete p;
+            cout << "프로그램 종료." << endl;
+            break;
+        }
+        else {
+            cout << "잘못된 선택입니다." << endl;
+        }
     }
 
     return 0;
